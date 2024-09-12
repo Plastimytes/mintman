6f64ec0e26001e3268d0405ac132573480c119d7
Plastimytes <magezirichardelijah@gmail.com>
2024-09-12 17:55:50 +0300

    Add files via upload

---
+++ first javascript.js
@@ -0,0 +1,6 @@
+//This is my first Js application
+console.log("Hello World")
+console.log("Magezi Richard Elijah")
+console.log("B27258")
+console.log("BSIT")
+//Python : print()
---
+++ variables.js
@@ -0,0 +1,32 @@
+//Iinitialisation
+var my_name="Magezi Richard"
+
+//Declare a variable
+var height;//var is old school method of assigning
+
+//Assign a variable
+height= 3.4;
+
+console.log(my_name);
+console.log(height);
+
+//let
+let area = 90.5;//keywords are highlighted ie; Var and Let
+
+let f_name;//let is better
+
+f_name="Elijah"
+
+console.log(area);
+console.log(f_name);
+console.log(area,"-",f_name)//concatenate
+
+//const
+//Initialize
+const pie = 3.14;
+
+//Declare
+
+
+
+
