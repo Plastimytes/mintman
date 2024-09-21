1dd9caec5b10fd8f695ed0c0d35bf0a2cab05d45
Plastimytes <magezirichardelijah@gmail.com>
2024-09-21 09:51:16 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-"Hello World"
+console.log("Hello World");
+
+let first_name="Jane";
+let last_name="Doe";
+
+const PIE = 3.13243;
+const MAX_SPEED = 4000; //Const names must be in capital lettters//Const makes sure the variable remains the same
+
+var school_name="UCU"; //Var is old school//
+
+//One line comment
+/*
+Multiple line comments
+*/
+
+///THE CODE QUEST OF THE TECH QUEENS///
+//Zara's Vault of Variables//
+let queeenName="Zara";
+let completedChallenges=0;
+ completedChallenges=completedChallenges+1;
+ console.log(completedChallenges);
+
+//Amina's Amazing Functions//
+function celebrateWin(challengeName){
+    console.log("Challenge:"+ challengeName +"completed!")
+    console.log("Time to take the next challenge")
+}
+
+celebrateWin("Fire Challenge")
+
+//Linas legendary loops//
+for (let i=1; i<=3; i+=1){
+    console.log("The Tech Queens have solved challenge X")
+}
+
+let nextChallenge = "loops";
+if(nextChallenge==="loops"){
+    console.log("Time to practice loops")
+}
+if (nextChallenge ==="functions") {
+    console.log("Time to master functions")
+    
+} 
+if (nextChallenge !=="functions" & nextChallenge !=="loops") {
+    console.log("On to the next challenge")
+}
