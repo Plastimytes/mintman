ea7b7ade3674018a3f7ce07cba52b7103951299f
Plastimytes <magezirichardelijah@gmail.com>
2024-09-29 15:02:54 +0300

    Add files via upload

---
+++ Fire_breathing_bug_Update.js
@@ -0,0 +1,24 @@
+console.log("The world is a buggy program");
+console.log ("So it cries out in flame")
+
+let first_name="Children_of_Sabaramond"
+let location="Other_side_of_the_sky"
+if (first_name=="Children of Sabaramond");{
+    console.log("BURN!!!@#$%^&*()")
+
+}
+let Document="CONTROL"
+
+if (document=="CONTROL"){
+    console.log("Controlled by the sky")
+}
+
+console.log("The world is a buggy program")
+
+let second_name="Children_of_Sabaramond"
+
+if (second_name=="Children of Sabaramond"){
+    console.log("BURN!!!@#$%^&*()")
+
+}
+
