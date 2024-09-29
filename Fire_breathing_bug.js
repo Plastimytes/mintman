d371da741ac78f5c0cb168d1115f5bf206aa87c3
Plastimytes <magezirichardelijah@gmail.com>
2024-09-29 14:52:08 +0300

    Add files via upload

---
+++ Fire_breathing_bug.js
@@ -0,0 +1,11 @@
+console.log("The world is a buggy program");
+console.log ("So it cries out in flame")
+
+let first_name="Children_of_Sabaramond"
+let location="Other_side_of_the_sky"
+if (first_name=="Children of Sabaramond");{
+    console.log("BURN!!!@#$%^&*()")
+
+}
+let Document="CONTROL"
+
