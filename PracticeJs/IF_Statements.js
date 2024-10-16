1af982ff90a03e1d2fd22eb84813eb2e7f4f7576
Plastimytes <magezirichardelijah@gmail.com>
2024-10-16 21:34:08 +0300

    Add files via upload

---
+++ PracticeJs/IF_Statements.js
@@ -0,0 +1,24 @@
+let age=10;
+if(age<12){
+    console.log("RUn home");
+}else{
+    console.log("Come on in")
+}
+
+let country = "Damascus";
+let Age=40;
+if(Age>=20 && country=="Damascus"){
+    console.log("DO YOU NEED FOOD")
+}else{
+    console.log("Please seek counseling")
+}
+
+//If else
+let comm_age =15;
+if (comm_age >= 20){
+    console.log("You can get married")
+}else if (comm_age >=10 && comm_age <= 20){
+    console.log("You are still a teenager, dont even think about it !!")
+}else{
+    console.log("Why are you even here!!")
+}
