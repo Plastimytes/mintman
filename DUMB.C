5b02888dae3a94158de70ef134fe492e2e0443ee
Magezi Richard Elijah <magezirichardelijah@gmail.com>
2025-02-03 16:20:15 +0300

    Add files via upload

---
+++ DUMB.C
@@ -0,0 +1,33 @@
+main() {}
+
+//Declaring main()as an int
+int main() {}
+
+///Sending a value back to the function
+int main() {return(0);}//Semi colon acts as a fullstop
+
+///It can also look like this
+int main() 
+{
+    return(0);
+}
+
+///Making the code do something
+#include <stdio.h>
+int main() 
+{
+    puts("Greetings human")//'puts' displays text on the screen
+
+
+    return(0);
+}
+
+//The c skeleton
+#include <something.h>
+
+int main()
+{
+    statement:
+    statement:
+    return(0);
+}
