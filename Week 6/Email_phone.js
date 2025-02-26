827557c1b7adddf3f1487a2bbbeb5adda98b68e6
Magezi Richard Elijah <magezirichardelijah@gmail.com>
2025-02-26 18:30:48 +0300

    Update Email_phone.js

--- Week 6/Email_phone.js
+++ Week 6/Email_phone.js
@@ -1,5 +1,6 @@
 var email ="magezirichardelijah@gmail.com";
 var phone = "078823415";
+
 let password = "1234";
 if ((email =="magezirichardelijah@gmail.com"||phone=="078823415")&& password == 1234){
     console.log("Procceed");
